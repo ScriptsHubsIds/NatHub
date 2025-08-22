f9cc25a9057fd7eb0e785b2183206cdc989a486f
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-08-22 11:01:14 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,13 +1,1 @@
-local function bypass()
-    ID="b0235f3a-fee6-4c84-b7ec-7cd06c976ba6";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
-
-end
-local function loadui()
-  
 loadstring(game:HttpGet("https://get.nathub.xyz/loader"))()
-
-end
-
-task.spawn(bypass)
-task.spawn(loadui)
